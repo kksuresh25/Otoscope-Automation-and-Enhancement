64d611dcf57b098f1bf8593a7eb5a19f9535355c
Jonathan Mairena <32823874+JonathanMairena@users.noreply.github.com>
2020-11-09 12:55:49 -0500

    Add files via upload

---
+++ alexnet_Classifier.py
@@ -0,0 +1,128 @@
+from sklearn.metrics import confusion_matrix
+import torch
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import torchvision.models as models
+import os 
+ 
+
+transformations = transforms.Compose([
+    transforms.Resize(255),
+    transforms.CenterCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(20,resample= Image.BILINEAR),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+train_set = datasets.ImageFolder(r"/Users/jonathanmairena/Documents/Fall2020/SeniorDesign/OtoscopeImages/Training", transform = transformations)
+
+test_set = datasets.ImageFolder(r"/Users/jonathanmairena/Documents/Fall2020/SeniorDesign/OtoscopeImages/Testing", transform = transformations)
+
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=300, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=20, shuffle=True)
+
+trainimages, trainlabels = next(iter(train_loader))
+testimages, testlabels = next(iter(test_loader))
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+num_epochs = 5
+num_classes = 4
+batch_size = 20
+learning_rate = 0.001
+    
+    
+# Initialize the prediction and label lists(tensors)
+predlist=torch.zeros(0,dtype=torch.long, device='cpu')
+lbllist=torch.zeros(0,dtype=torch.long, device='cpu')
+
+
+model = models.alexnet() 
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr = learning_rate)
+
+# keeping-track-of-losses 
+train_losses = []
+valid_losses = []
+
+for epoch in range(1, num_epochs + 1):
+    # keep-track-of-training-and-validation-loss
+    train_loss = 0.0
+    valid_loss = 0.0
+    correct_train = 0
+    correct_test = 0
+    total_train = 0
+    total_test = 0
+    # training-the-model
+    model.train()
+    for data, target in train_loader:
+        # move-tensors-to-GPU 
+        data = data.to(device)
+        target = target.to(device)
+        
+        # clear-the-gradients-of-all-optimized-variables
+        optimizer.zero_grad()
+        # forward-pass: compute-predicted-outputs-by-passing-inputs-to-the-model
+        output = model(data)
+        # calculate-the-batch-loss
+        loss = criterion(output, target)
+        # backward-pass: compute-gradient-of-the-loss-wrt-model-parameters
+        loss.backward()
+        # perform-a-ingle-optimization-step (parameter-update)
+        optimizer.step()
+        # update-training-loss
+        train_loss += loss.item() * data.size(0)
+        
+        #Accuracy 
+        _, predicted = torch.max(output.data, 1)
+        total_train += target.nelement()
+        correct_train += predicted.eq(target.data).sum().item()
+        train_accuracy = 100 * correct_train / total_train
+        
+    # validate-the-model
+    model.eval()
+    for data, target in test_loader:
+        
+        data = data.to(device)
+        target = target.to(device)
+        
+        output = model(data)
+        
+        loss = criterion(output, target)
+        
+        # update-average-validation-loss 
+        valid_loss += loss.item() * data.size(0)
+        
+        #Accuracy 
+        _, predicted = torch.max(output.data, 1)
+        total_test += target.nelement()
+        correct_test += predicted.eq(target.data).sum().item()
+        test_accuracy = 100 * correct_test / total_test
+        
+        #Confusion Matrix
+        predlist=torch.cat([predlist,predicted.view(-1).cpu()])
+        lbllist=torch.cat([lbllist,target.view(-1).cpu()])       
+            
+
+    # calculate-average-losses
+    train_loss = train_loss/len(train_loader.sampler)
+    valid_loss = valid_loss/len(test_loader.sampler)
+    train_losses.append(train_loss)
+    valid_losses.append(valid_loss)
+        
+    # print-training/validation-statistics 
+    print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f} \tTrain Accuracy: {:.6f} \tTest Accuracy: {:.6f}'.format(epoch, train_loss, valid_loss, train_accuracy, test_accuracy) )
+
+conf_mat=confusion_matrix(lbllist.numpy(), predlist.numpy())
+print(conf_mat)
+
+    #print('Epoch: {} \tTraining Loss: {:.6f}'.format(
+    #    epoch, train_loss))
